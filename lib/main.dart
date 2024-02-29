f1433c7324b7fc63f0c2884fb9b4f74ca349b9eb
Noor Ul Islam <noorulislam770@gmail.com>
2024-02-29 19:00:36 +0500

    using arrow functions and using rows and colums alongside their alignments

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: UserButton(),
-      ),
-    ),
-  ));
-}
+void main() => runApp(MaterialApp(
+      home: MyWidget(),
+    ));
+// void main() {
+//   runApp(MaterialApp(
+//     home: Scaffold(
+//       body: MyWidget(),
+//     ),
+//   ));
+// }
 
-class UserButton extends StatelessWidget {
+class MyWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => {print("you have tapped the screen.")},
-      child: Container(
-        height: 60,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5.0), color: Colors.red[600]),
-        child: const Center(
-          child: Text('Click Here'),
-        ),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        body: Container(color: Colors.red),
+      );
 }
