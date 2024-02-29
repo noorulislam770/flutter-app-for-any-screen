c4204852ea6f562da74181d30c335cc506f7f503
Noor Ul Islam <noorulislam770@gmail.com>
2024-02-29 13:35:51 +0500

    Created a stateless widget with text in center and a gestrue detector that prints clicked on the screen when a click is perormend

---
+++ lib/main_stateless_widgets.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme:
+        ThemeData(brightness: Brightness.dark, primaryColor: Colors.blueGrey),
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hello susum new"),
+      ),
+      body: const Center(
+        child: Text('Some text in center. edited'),
+      ),
+    );
+  }
+}
+
+// void main() {
+//   runApp(MaterialApp(
+//     theme:
+//         ThemeData(brightness: Brightness.dark, primaryColor: Colors.blueGrey),
+//     home: Scaffold(
+//       appBar: AppBar(
+//         title: const Text("App Bar Title "),
+//       ),
+//       body: const Center(
+//         child: Text('This is some Text'),
+//       ),
+//     ),
+//   ));
+// }
